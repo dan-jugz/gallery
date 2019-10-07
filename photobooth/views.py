da4c5cb0afb3876937bef1aa8328121ea14e71f7
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 08:15:59 +0300

    setup the basic route and ddefine a function that takes the user to homepage

--- photobooth/views.py
+++ photobooth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import datetime as dt
+from .models import Image
 
 # Create your views here.
+def images_of_day(request):
+    date =dt.date.today()
+    photos=Image.objects.all()
+    
+    return render(request, "homepage.html",{"date":date,"photos":photos})
