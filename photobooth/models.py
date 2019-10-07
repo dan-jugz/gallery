fc4aae82615577c40e4c1b46fddea8519e2a69bc
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 08:48:00 +0300

    definee the methods for saving and deleting

--- photobooth/models.py
+++ photobooth/models.py
@@ -12,6 +12,11 @@ class User(models.Model):
     def __str__(self):
         return self.first_name
 
+    def save_user(self):
+        self.save()
+
+    class Meta:
+        ordering = ['first_name']
 
 class Location(models.Model):
     loc_name=models.CharField(max_length=100)
@@ -19,12 +24,26 @@ class Location(models.Model):
     def __str__(self):
         return self.loc_name
 
+    # method to save location to database
+    def save_loc(self):
+        self.save()
+
+    # method to delete a category from db  
+    def delete_loc(self): 
+        self.delete()
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category_name
 
+    # method to save a category to db
+    def save_category(self):
+        self.save() 
+
+    # method to delete a category from db
+    def delete_category(self):
+        self.delete()
 
 class tags(models.Model):
     tag_name=models.CharField(max_length=30, default='tag_name')
@@ -38,9 +57,10 @@ class Image(models.Model):
     post = models.CharField(max_length =100)
     img_loc = models.ForeignKey(Location,on_delete=models.DO_NOTHING)
     img_category = models.ForeignKey(Category,on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to = 'images/',default='')
+    image = models.ImageField(upload_to = 'images/')
 
     def __str__(self):
         return f'Image{self.title}-{self.img_loc}-{self.img_category}-{self.pub_date}'
