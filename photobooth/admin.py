82d88b4098ae3483d620ce7f787919fae245a19a
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 11:23:02 +0300

    register the various classes onto the admin interface

--- photobooth/admin.py
+++ photobooth/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Location,Category,Image,tags,User
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags',)
+
+admin.site.register(User)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image,ImageAdmin)
+admin.site.register(tags)
