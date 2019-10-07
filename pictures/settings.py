6f23a03b804560f4877b16a6c1c6d50b43500066
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 17:45:46 +0300

    deployment

--- pictures/settings.py
+++ pictures/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-django_heroku.settings(locals())
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
