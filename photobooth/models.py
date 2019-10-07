85d2870d0d302d6ca49752d36121bea8359297b5
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 08:24:52 +0300

    add the image model

--- photobooth/models.py
+++ photobooth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.CharField(max_length =100)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/',default='')
