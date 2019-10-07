fa2fc5cd105d431e413e7c773cdb5f1ed1d1cc72
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 09:24:23 +0300

    set the image test class

--- photobooth/tests.py
+++ photobooth/tests.py
@@ -16,3 +16,23 @@ class UserTestClass(TestCase):
         self.daniel.save_user()
         users = User.objects.all()
         self.assertTrue(len(users) > 0)
+
+
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.daniel= User(first_name = 'Daniel', last_name ='Njuguna', email ='njuguna@moringaschool.com')
+        self.daniel.save_user()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_image= Image(title = 'Test Image',post = 'This is a random test Post',user = self.daniel)
+        self.new_image.save()
+
+        self.new_image.tags.add(self.new_tag)
+
+
