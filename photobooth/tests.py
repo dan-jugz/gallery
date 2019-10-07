f438aebd11c6c0361f27a4b32cd5ba098a8c29e1
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 09:01:58 +0300

    test the user class

--- photobooth/tests.py
+++ photobooth/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import User,Image,tags
+import datetime as dt
 
 # Create your tests here.
+class UserTestClass(TestCase):
+# Set up method
+    def setUp(self):
+        self.daniel= User(first_name = 'Daniel', last_name ='Njuguna', email ='njuguna@moringaschool.com')
+    
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.daniel,User))
+
+    def test_save_method(self):
+        self.daniel.save_user()
+        users = User.objects.all()
+        self.assertTrue(len(users) > 0)
