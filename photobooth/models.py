08cbab2cb51491a5d057a6665b704b83b40d425d
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 08:27:40 +0300

    add user class to the model

--- photobooth/models.py
+++ photobooth/models.py
@@ -7,3 +7,16 @@ class Image(models.Model):
     post = models.CharField(max_length =100)
     pub_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to = 'images/',default='')
+
+    def __str__(self):
+        return f'Image{self.title}-{self.img_loc}-{self.img_category}-{self.pub_date}'
+    
+
+class User(models.Model):
+    first_name = models.CharField(max_length =20)
+    last_name = models.CharField(max_length =20)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return self.first_name
