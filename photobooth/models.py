66f2d0d9563060794dbc33d882b8a0c526f42a79
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-07 08:34:05 +0300

    add category to image

--- photobooth/models.py
+++ photobooth/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 import datetime as dt
 
+
 # Create your models here.
-class Image(models.Model):
-    title = models.CharField(max_length =60)
-    post = models.CharField(max_length =100)
-    img_loc = models.ForeignKey(Location,on_delete=models.DO_NOTHING)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to = 'images/',default='')
-
-    def __str__(self):
-        return f'Image{self.title}-{self.img_loc}-{self.img_category}-{self.pub_date}'
-    
-
 class User(models.Model):
     first_name = models.CharField(max_length =20)
     last_name = models.CharField(max_length =20)
@@ -29,4 +19,21 @@ class Location(models.Model):
     def __str__(self):
         return self.loc_name
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.category_name
+
+
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.CharField(max_length =100)
+    img_loc = models.ForeignKey(Location,on_delete=models.DO_NOTHING)
+    img_category = models.ForeignKey(Category,on_delete=models.DO_NOTHING)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/',default='')
+
+    def __str__(self):
+        return f'Image{self.title}-{self.img_loc}-{self.img_category}-{self.pub_date}'
+    
