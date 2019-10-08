4f22582c2854ccd61e901b7e9b8e479db19e7e10
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-08 10:52:39 +0300

    set a modal to copy the link from

--- static/js/galleria.js
+++ static/js/galleria.js
@@ -5,3 +5,12 @@ var $grid = $('.grid').imagesLoaded( function() {
         columnWidth:'.grid-sizer'
     });
   });
+
+  $(function() {
+    $("input").click(function() {
+    $(this).focus();
+    $(this).select();
+    document.execCommand('copy');
+    $(this).after("Copied link");
+    });
+   });
